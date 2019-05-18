788f9d39f63128f52e63dcea4c0965b32e02772a
Ashley Sands <ashsands1@amstrike.com>
2019-05-18 02:40:49 +0100

    added pancake
    and prevented its RB from sleeping.
    
    it was not moving witht eh frying pan :|

---
+++ SimPancake 3000/Assets/Scripts/Objects/Pancake.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pancake : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //never sleep :)
+        if(rb.IsSleeping())
+        {
+            print("Stop sleeping on the job!");
+            rb.WakeUp();
+        }
+
+    }
+}
