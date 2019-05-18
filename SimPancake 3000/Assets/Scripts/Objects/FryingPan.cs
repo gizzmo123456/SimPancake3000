1cb8d6187a5a56307022058cf04dbca77f17e7d7
Ashley Sands <ashsands1@amstrike.com>
2019-05-18 02:40:18 +0100

    Added Frying pan can basic movement
    and prevented pancake from sleeping

---
+++ SimPancake 3000/Assets/Scripts/Objects/FryingPan.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FryingPan : MonoBehaviour
+{
+    [SerializeField]
+    private int panID = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        InputValues inputs = InputHandler.GetInputs();
+
+        //TODO: check that the panID is in range of pan count once we have a Static Game Class
+        if (panID < 0 || panID >= 3) //i know there 3 but still.
+        {
+            Debug.LogError("Pan Id Out of range (obj name: "+name+")");
+            return;
+        }
+        int x = inputs.pans_x[panID];
+        int y = inputs.pans_y[panID];
+
+        transform.eulerAngles = new Vector3(-x, -90, -y);
+
+
+    }
+}
