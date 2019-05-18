2ccebe834a6784870b509e589092189a38046db0
Ashley Sands <ashsands1@amstrike.com>
2019-05-18 04:57:46 +0100

    added pan distance to frying pans

--- SimPancake 3000/Assets/Scripts/Objects/FryingPan.cs
+++ SimPancake 3000/Assets/Scripts/Objects/FryingPan.cs
@@ -10,10 +10,18 @@ public class FryingPan : MonoBehaviour
 
     Pancake currentPancake;
 
+    private float startYPosition;
+    [SerializeField]
+    private float panPositionOffset = 10f;
+
+    [SerializeField]
+    private float minInputValue = 230;
+    [SerializeField]
+    private float maxInputValue = 1023f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startYPosition = transform.position.y;
     }
 
     // Update is called once per frame
@@ -32,11 +40,16 @@ public class FryingPan : MonoBehaviour
         int x = inputs.pans_x[panID];
         int y = inputs.pans_y[panID];
 
+        Vector3 position = transform.position;
+        print(inputs.panDistances[panID]);
+        position.y = startYPosition + (panPositionOffset * (1f - ((inputs.panDistances[panID] - minInputValue) / (maxInputValue - minInputValue))));
+
         // make shore the pancake is awake if the inputs have changed since the last frame :)
         if (currentPancake != null && (x != last_x || y != last_y))
             currentPancake.WakeUp();
 
         transform.eulerAngles = new Vector3(-x, -90, -y);
+        transform.position = position;
 
         last_x = x;
         last_y = y;
