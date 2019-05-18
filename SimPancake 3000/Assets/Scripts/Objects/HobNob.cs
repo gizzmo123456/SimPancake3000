0d38d8177730e82cc0a8648c1e192653a6c79b5d
Ashley Sands <ashsands1@amstrike.com>
2019-05-18 03:53:17 +0100

    Added basic hobNob script

---
+++ SimPancake 3000/Assets/Scripts/Objects/HobNob.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HobNob : MonoBehaviour
+{
+    [SerializeField]
+    private int hobID = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        InputValues inputs = InputHandler.GetInputs();
+
+        //TODO: check that the panID is in range of pan count once we have a Static Game Class
+        if (hobID < 0 || hobID >= 3) //i know there 3 but still.
+        {
+            Debug.LogError("Pan Id Out of range (obj name: " + name + ")");
+            return;
+        }
+
+        Vector3 currentRotation = transform.eulerAngles;
+
+        currentRotation.z = -270f * (inputs.hobs[hobID] / 1023f);
+        transform.eulerAngles = currentRotation;
+        print(inputs.hobs[hobID]);
+
+    }
+}
