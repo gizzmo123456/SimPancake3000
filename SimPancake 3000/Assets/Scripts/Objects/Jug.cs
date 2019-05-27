09c2f5d71fcd1cda8addc655cba51183616176ba
Ashley Sands <ashsands1@amstrike.com>
2019-05-27 02:26:24 +0100

    stoped batter rotating

--- SimPancake 3000/Assets/Scripts/Objects/Jug.cs
+++ SimPancake 3000/Assets/Scripts/Objects/Jug.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using AMS_Helpers;
 
 public class Jug : MonoBehaviour
 {
+
+	[SerializeField] private Transform batter;
+
+	[SerializeField] private MinMax MinMaxRotation = new MinMax(-10, 90);
+	[SerializeField] private MinMax MinMaxInputValue = new MinMax(-10, 90);
 
 	[SerializeField] private Vector3 defaultPosition = Vector3.zero;
 	[SerializeField] private Transform[] fryingPans;
@@ -24,8 +30,15 @@ public class Jug : MonoBehaviour
 		Vector3 rotation = transform.eulerAngles;
 
 		rotation.x = inputs.jug;
-
+		print( inputs.jug );
 		transform.eulerAngles = rotation;
+
+		// keep the batters x rotation @ 0 so its always level.
+		// TODO: make dynamic.
+		Vector3 batterRotation = batter.eulerAngles;
+
+		batterRotation.x = 0;
+		batter.eulerAngles = batterRotation;
 
 	}
 
