63b76df421265071184ef41363a551f8e143f9ba
Ashley Sands <ashsands1@amstrike.com>
2019-05-29 22:57:27 +0100

    Put the Struct back inside the pancakeMeshCtrl since its not backwards capable
    This will make a bit more sence in a feture commit, but basicly there will be a change in physicsBall with is no compatible with VericeGroup from PancakeMeshCntr
    
    So i have made a copy into with the PB updated in the new PancakeMeshController (yet to be commited tho, you'll see future me.)

--- SimPancake 3000/Assets/Scripts/Objects/PancakePhysics/PancakeMeshCtrl.cs
+++ SimPancake 3000/Assets/Scripts/Objects/PancakePhysics/PancakeMeshCtrl.cs
@@ -9,183 +9,182 @@ using UnityEngine;
 public class PancakeMeshCtrl : MonoBehaviour
 {
 
-    private Mesh mesh;
-    [SerializeField]
-    private Vector3[] vertices;
+	private Mesh mesh;
+	[SerializeField]
+	private Vector3[] vertices;
 
-    public List<VerticeGroup> verticeGroups;
+	public List<VerticeGroup> verticeGroups;
 
-    [SerializeField]
-    private PancakePhysicsBall physicsBall;
-    int centerId = -1;  //so we can set it on physics ball later
+	[SerializeField]
+	private PancakePhysicsBall physicsBall;
+	int centerId = -1;  //so we can set it on physics ball later
 
-    void Start()
-    {
-        mesh = GetComponent<MeshFilter>().mesh;
-        vertices = mesh.vertices;
-        verticeGroups = new List<VerticeGroup>();
+	void Start()
+	{
+		mesh = GetComponent<MeshFilter>().mesh;
+		vertices = mesh.vertices;
+		verticeGroups = new List<VerticeGroup>();
 
 
-        bool found = false;
-       
-        /*
+		bool found = false;
+
+		/*
          * group all vert's, so anythink that has the same x, z value can be moved together.
          * we are also going to put a point (or sphere) on each group which will be used for are physics
          * we want the two faces of the cylinder (or pancake) to move together.
          */
 
-        // loop all vert's
-        for ( int i = 0; i < vertices.Length; i++ )
-        {
-            found = false;
+		// loop all vert's
+		for ( int i = 0; i < vertices.Length; i++ )
+		{
+			found = false;
 
-            // find if there is an existing group
-            for( int j = 0; j < verticeGroups.Count; j++ )
-            {
-                if (verticeGroups[j].Compare(vertices[i].x, vertices[i].z))
-                {
-                    verticeGroups[ j ].AddVertID( i );
-                    found = true;
+			// find if there is an existing group
+			for ( int j = 0; j < verticeGroups.Count; j++ )
+			{
+				if ( verticeGroups[ j ].Compare( vertices[ i ].x, vertices[ i ].z ) )
+				{
+					verticeGroups[ j ].AddVertID( i );
+					found = true;
 
-                    break;
-                }
-            }
-
-            
-            if( !found ) // add spwan physics ball and add new group.
-            {
-                PancakePhysicsBall pb = Instantiate( physicsBall, transform );
-                verticeGroups.Add( new VerticeGroup( i, vertices[ i ].x, vertices[ i ].z, pb) );
-
-                pb.transform.localPosition = verticeGroups[ verticeGroups.Count - 1 ].GetAsVector3();
-
-                // update center id once it has been found (it should always be 0,0)
-                if ( centerId == -1 && vertices[ i ].x == 0 && vertices[ i ].z == 0 )
-                {
-                    centerId = verticeGroups.Count - 1;
-                }
-            }
-
-        }
+					break;
+				}
+			}
 
 
-        verticeGroups[ centerId ].physicsBall.SetIsCenter( true );
-        // now we have all our groups we can, set the center and two cloest physics balls to each physics ball
-        for ( int i = 0; i < verticeGroups.Count; i++ )
-        {
-            verticeGroups[ i ].physicsBall.SetBall( PancakePhysicsBall.BallType.center, verticeGroups[ centerId ].physicsBall );
-            // find the two closes balls that are not the center.
-            List<int> cloestId = new List<int>( 2 );
-            cloestId.AddRange( new int[ 2 ] { 0, 0 } );
+			if ( !found ) // add spwan physics ball and add new group.
+			{
+				PancakePhysicsBall pb = Instantiate( physicsBall, transform );
+				verticeGroups.Add( new VerticeGroup( i, vertices[ i ].x, vertices[ i ].z, pb ) );
 
-            List<float> dist = new List<float>( 2 );
-            dist.AddRange( new float[ 2 ] { 100f, 100f } );
+				pb.transform.localPosition = verticeGroups[ verticeGroups.Count - 1 ].GetAsVector3();
 
-            verticeGroups[ i ].physicsBall.name = i + "";
-            print( i + " center dist: " + Vector3.Distance( verticeGroups[ i ].GetAsVector3(), verticeGroups[ centerId ].GetAsVector3() ) );
+				// update center id once it has been found (it should always be 0,0)
+				if ( centerId == -1 && vertices[ i ].x == 0 && vertices[ i ].z == 0 )
+				{
+					centerId = verticeGroups.Count - 1;
+				}
+			}
+
+		}
 
 
-            for ( int j = 0; j < verticeGroups.Count; j++ )
-            {
+		verticeGroups[ centerId ].physicsBall.SetIsCenter( true );
+		// now we have all our groups we can, set the center and two cloest physics balls to each physics ball
+		for ( int i = 0; i < verticeGroups.Count; i++ )
+		{
+			verticeGroups[ i ].physicsBall.SetBall( PancakePhysicsBall.BallType.center, verticeGroups[ centerId ].physicsBall );
+			// find the two closes balls that are not the center.
+			List<int> cloestId = new List<int>( 2 );
+			cloestId.AddRange( new int[ 2 ] { 0, 0 } );
 
-                if ( verticeGroups[ j ].GetAsVector3() == Vector3.zero || i == j ) // skip center and self
-                    continue;
+			List<float> dist = new List<float>( 2 );
+			dist.AddRange( new float[ 2 ] { 100f, 100f } );
 
-                float tempDist = Vector3.Distance( verticeGroups[ i ].GetAsVector3(), verticeGroups[ j ].GetAsVector3() );
-                if ( j == 0 || tempDist < dist[ 0 ] )
-                {
-                    cloestId.Insert( 0, j );
-                    dist.Insert( 0, tempDist );
-                }
-                else if( j == 1 || tempDist < dist[1] )
-                {
-                    cloestId.Insert( 1, j );
-                    dist.Insert( 1, tempDist );
-                }
+			verticeGroups[ i ].physicsBall.name = i + "";
+			print( i + " center dist: " + Vector3.Distance( verticeGroups[ i ].GetAsVector3(), verticeGroups[ centerId ].GetAsVector3() ) );
 
-            }
 
-            // set the two balls now we have found the cloest two
-            verticeGroups[ i ].physicsBall.SetBall( PancakePhysicsBall.BallType.left, verticeGroups[ cloestId[0] ].physicsBall );
-            verticeGroups[ i ].physicsBall.SetBall( PancakePhysicsBall.BallType.right, verticeGroups[ cloestId[1] ].physicsBall );
-            verticeGroups[ i ].physicsBall.Init();
+			for ( int j = 0; j < verticeGroups.Count; j++ )
+			{
 
-        }
-        
+				if ( verticeGroups[ j ].GetAsVector3() == Vector3.zero || i == j ) // skip center and self
+					continue;
 
-    }
+				float tempDist = Vector3.Distance( verticeGroups[ i ].GetAsVector3(), verticeGroups[ j ].GetAsVector3() );
+				if ( j == 0 || tempDist < dist[ 0 ] )
+				{
+					cloestId.Insert( 0, j );
+					dist.Insert( 0, tempDist );
+				}
+				else if ( j == 1 || tempDist < dist[ 1 ] )
+				{
+					cloestId.Insert( 1, j );
+					dist.Insert( 1, tempDist );
+				}
 
-    void FixedUpdate()
-    {
-        //return;
-        for ( int i = 0; i < verticeGroups.Count; i++ )
-        {
-            Vector3 pbPosition = verticeGroups[ i ].physicsBall.transform.localPosition;
+			}
 
-            verticeGroups[ i ].Update( ref vertices, pbPosition.x, pbPosition.y, pbPosition.z );
-        }
-    }
+			// set the two balls now we have found the cloest two
+			verticeGroups[ i ].physicsBall.SetBall( PancakePhysicsBall.BallType.left, verticeGroups[ cloestId[ 0 ] ].physicsBall );
+			verticeGroups[ i ].physicsBall.SetBall( PancakePhysicsBall.BallType.right, verticeGroups[ cloestId[ 1 ] ].physicsBall );
+			verticeGroups[ i ].physicsBall.Init();
 
-    private void Update()
-    {
-        mesh.vertices = vertices;   // << am i fucking crazy
-        //mesh.RecalculateBounds();
-        //mesh.RecalculateNormals();
-    }
+		}
 
-}
 
-[System.Serializable]
-public struct VerticeGroup
-{
-	private float x, z;   // we dont need the Y asix since we only need to change the shape of its face and the top will have a matching bottom.
-	private List<int> verticesIDs;
-	public PancakePhysicsBall physicsBall;
-	public bool isCenter;
-
-	public VerticeGroup( int vertID, float xAxis, float zAxis, PancakePhysicsBall pb )
-	{
-		x = xAxis;
-		z = zAxis;
-		physicsBall = pb;
-		verticesIDs = new List<int>();
-		verticesIDs.Add( vertID );
-		isCenter = false;
 	}
 
-	public void Update( ref Vector3[] verts, float xAxis, float yAxis, float zAxis )
+	void FixedUpdate()
 	{
-
-		x = xAxis;
-		z = zAxis;
-
-		foreach ( int vid in verticesIDs )
+		//return;
+		for ( int i = 0; i < verticeGroups.Count; i++ )
 		{
-			verts[ vid ].x = x;
-			verts[ vid ].z = z;
-			verts[ vid ].y = yAxis;
+			Vector3 pbPosition = verticeGroups[ i ].physicsBall.transform.localPosition;
+
+			verticeGroups[ i ].Update( ref vertices, pbPosition.x, pbPosition.y, pbPosition.z );
+		}
+	}
+
+	private void Update()
+	{
+		mesh.vertices = vertices;   // << am i fucking crazy
+									//mesh.RecalculateBounds();
+									//mesh.RecalculateNormals();
+	}
+
+	[System.Serializable]
+	public struct VerticeGroup
+	{
+		private float x, z;   // we dont need the Y asix since we only need to change the shape of its face and the top will have a matching bottom.
+		private List<int> verticesIDs;
+		public PancakePhysicsBall physicsBall;
+		public bool isCenter;
+
+		public VerticeGroup( int vertID, float xAxis, float zAxis, PancakePhysicsBall pb )
+		{
+			x = xAxis;
+			z = zAxis;
+			physicsBall = pb;
+			verticesIDs = new List<int>();
+			verticesIDs.Add( vertID );
+			isCenter = false;
+		}
+
+		public void Update( ref Vector3[] verts, float xAxis, float yAxis, float zAxis )
+		{
+
+			x = xAxis;
+			z = zAxis;
+
+			foreach ( int vid in verticesIDs )
+			{
+				verts[ vid ].x = x;
+				verts[ vid ].z = z;
+				verts[ vid ].y = yAxis;
+			}
+
+		}
+
+		public void AddVertID( int id )
+		{
+			verticesIDs.Add( id );
+		}
+
+		public Vector3 GetAsVector3()
+		{
+			return new Vector3( x, 0, z );
+		}
+
+		public void SetCenter( bool b )
+		{
+			isCenter = b;
+		}
+
+		public bool Compare( float xAxis, float zAxis )
+		{
+			return x == xAxis && z == zAxis;
 		}
 
 	}
-
-	public void AddVertID( int id )
-	{
-		verticesIDs.Add( id );
-	}
-
-	public Vector3 GetAsVector3()
-	{
-		return new Vector3( x, 0, z );
-	}
-
-	public void SetCenter(bool b)
-	{
-		isCenter = b;
-	}
-
-	public bool Compare( float xAxis, float zAxis )
-	{
-		return x == xAxis && z == zAxis;
-	}
-
 }
