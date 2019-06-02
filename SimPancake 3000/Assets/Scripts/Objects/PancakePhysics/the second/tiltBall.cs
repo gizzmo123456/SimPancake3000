5a57a647fedf1f1f7d88fd503ba726fcb643d933
Ashley Sands <ashsands1@amstrike.com>
2019-06-02 22:35:03 +0100

    Added tilit ball and added to frying pan
    the ball idles in the center of the pan and moved to the position inwhich the batter should move towards when spreading the batter out in the pan.

---
+++ SimPancake 3000/Assets/Scripts/Objects/PancakePhysics/the second/tiltBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tiltBall : MonoBehaviour
+{
+	[SerializeField] private Transform pan;
+	[SerializeField] private float range = 2; 
+
+	// Update is called once per frame
+	void Update()
+    {
+
+		InputValues inputs = InputHandler.GetInputs();
+
+		float x_pos = /*pan.position.x + */(( inputs.pans_x[ 0 ] / 180f ) * range);
+		float z_pos = /*pan.position.z + */(( inputs.pans_y[ 0 ] / 180f ) * range);
+
+		transform.localPosition = new Vector3( z_pos, x_pos, 0 );
+
+    }  
+}
