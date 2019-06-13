cc7b11c98128e1fe87398a05e1d4342eea7f543a
Ashley Sands <ashsands1@amstrike.com>
2019-06-13 17:11:58 +0100

    added struct to group together all objects / components that make you a cooking hob
    ie. hob knob, hob flame, frying pan ect...

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/HobGroup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A struct to group all of the required hob objects (ie. hob knob, hob flame, frying pan ect...) together.
+/// </summary>
+/// 
+[System.Serializable]
+public struct HobGroup
+{
+
+}
