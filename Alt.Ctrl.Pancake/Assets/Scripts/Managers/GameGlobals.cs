0e9a779cdcf86b9a82a2b7e95d2d7783ffdeeb8d
Ashley Sands <ashsands1@amstrike.com>
2019-06-13 17:09:31 +0100

    Inital commit for basic managers
    GameGlobals static class,
    Game Manager, manages overal game such as game state (ie. Menu, loading, Game ect.)
    Inputs, handles both serial and Mouse and keyboard
    Level Manager, manges each level ie the state

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Managers/GameGlobals.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Containes all data that is used globaly
+/// and maybe the odd shortcut.
+/// </summary>
+public static class GameGlobals
+{
+
+	// 
+	public static string name = "Alt.Ctrl.Pancake";
+	public static int buildId = 0;
+
+	// static store
+
+
+}
