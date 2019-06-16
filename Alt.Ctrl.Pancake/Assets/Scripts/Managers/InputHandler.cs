11714d51d96746b15f2beb485e34cd7d96ae1463
Ashley Sands <ashsands1@amstrike.com>
2019-06-16 22:53:08 +0100

    added input name to summary of GetInputValue

--- Alt.Ctrl.Pancake/Assets/Scripts/Managers/InputHandler.cs
+++ Alt.Ctrl.Pancake/Assets/Scripts/Managers/InputHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using System.IO.Ports;		// Access to serial
-using System.Threading;		// create a thread for the serial commication. (otherwise it slow as)
+using System.Threading;     // create a thread for the serial commication. (otherwise it slow as)
 
 
 /// <summary>
@@ -276,7 +276,9 @@ public class InputHandler : MonoBehaviour
 	}
 
 	/// <summary>
-	/// Gets the input value for input name
+	/// <para>Gets the input value for input name</para>
+	/// <para>Inputs: name[prefix] </para> 
+	/// <para>panX_[0 - 2], panY_[0 - 2], panDistance_[0 - 2], hobKnob[0 - 2], jug, whisk, panToggle, webcamX, webcamY</para>
 	/// </summary>
 	/// <param name="inputName">Name of input</param>
 	/// <param name="value">REF Value of input</param>
