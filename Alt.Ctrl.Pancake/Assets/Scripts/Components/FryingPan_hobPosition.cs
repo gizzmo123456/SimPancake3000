e46b63d8ee0ee1e7ae0af6f2ad9f6f6faf1c006f
Ashley Sands <ashsands1@amstrike.com>
2019-06-17 03:37:31 +0100

    added fryingPanPosition
    by its inputs

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/FryingPan_hobPosition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AMS_Helpers;
+
+
+public class FryingPan_hobPosition : BasePanGroup_singleInput
+{
+	[Header("Hob Position")]
+	[SerializeField] private MinMax yPositionRange = new MinMax( 0, 10 );
+
+    protected override void Update()
+    {
+
+		base.Update();
+
+		Vector3 position = transform.position;
+
+		position.y = yPositionRange.GetValue( inputValue.Precent );
+
+		transform.position = position;
+
+    }
+}
