e3d624c0cfc372bdbe7b695521ac9421499728c2
Ashley Sands <ashsands1@amstrike.com>
2019-06-18 00:15:26 +0100

    added class to blend the pour for jug batter

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/Jug_batterPourBlendShape.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AMS_Helpers;
+
+[RequireComponent(typeof(SkinnedMeshRenderer))]
+public class Jug_batterPourBlendShape : BasePanGroup_singleInput
+{
+	private SkinnedMeshRenderer skinnedMeshRenderer;
+
+	[Header( "Pour BlendShape" )]
+	[SerializeField] private int pour_blendShapeId = 0;
+
+	private void Awake()
+	{
+		skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+	}
+
+	protected override void Update()
+    {
+
+		base.Update();
+
+		float blendAmount = (1f - inputValue.ClampedPrecent) * 100f;
+
+		skinnedMeshRenderer.SetBlendShapeWeight( pour_blendShapeId, blendAmount );
+
+    }
+}
