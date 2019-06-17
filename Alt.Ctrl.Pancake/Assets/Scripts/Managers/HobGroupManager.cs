7ea2bb07b751e6d3d80668bb5b30ec3a4a17b450
Ashley Sands <ashsands1@amstrike.com>
2019-06-17 03:49:02 +0100

    added managers to scene

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Managers/HobGroupManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HobGroupManager : MonoBehaviour
+{
+
+	[SerializeField] private HobGroup[] hobGroups;
+	
+	private void Awake()
+	{
+		for ( int i = 0; i < hobGroups.Length; i++ )
+			hobGroups[ i ].SetHobGroupId( i );
+	}
+
+	public HobGroup GetHobGroup(int hobId)
+	{
+
+		return hobGroups[ hobId ];
+	}
+
+}
