42d2ba34779c165bba9e0b5edacc7a78b571620d
Ashley Sands <ashsands1@amstrike.com>
2019-06-17 03:37:07 +0100

    Added jug rotation

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/Jug_rotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AMS_Helpers;
+
+public class Jug_rotation : BasePanGroup_singleInput
+{
+	[Header("Jug Position")]
+	[SerializeField] private MinMax xRotationRange;
+
+    protected override void Update()
+    {
+
+		base.Update();
+
+		Vector3 rotation = Vector3.zero;
+
+		rotation.y = -90;	
+		rotation.x = xRotationRange.GetValue( inputValue.Precent );
+
+		transform.eulerAngles = rotation;
+
+    }
+}
