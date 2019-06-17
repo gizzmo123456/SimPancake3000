7a65b2bb9042c3f54daccfb29858de8858a1aecd
Ashley Sands <ashsands1@amstrike.com>
2019-06-17 01:27:13 +0100

    added BasePanGroup for multiple inputs

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/BasePanGroup_multipleInput.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AMS_Helpers;
+
+/// <summary>
+/// Base class for components that have multiple inputs.
+/// 
+/// </summary>
+public class BasePanGroup_multipleInput : BasePanGroup
+{
+
+	[Header( "Inputs" )]
+	[SerializeField] private string[] inputNames;
+	[SerializeField] private bool addPanId_prefix = false;
+	[SerializeField] protected MinMax[] inputValues;
+
+	protected string GetInputName( int inputId )
+	{
+		return inputNames[inputId] + ( addPanId_prefix ? "_" + PanGroupId : "" );
+	}
+
+	protected virtual void Awake()
+	{
+		if ( inputValues.Length == inputNames.Length )
+			Debug.LogError( GameGlobals.GetLogPrefix(name, GetType().ToString())+"Input put values and input names must be the same size" );
+	}
+
+	protected virtual void Update()
+	{
+
+		for ( int i = 0; i < inputNames.Length; i++ )
+			Inputs.GetInputValue( GetInputName( i ), ref inputValues[ i ].current );
+
+	}
+
+}
