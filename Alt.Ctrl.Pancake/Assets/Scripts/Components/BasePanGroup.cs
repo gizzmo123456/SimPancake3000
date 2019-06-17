81724d3157451c8671f53ac12ae1dd54e7a29d5c
Ashley Sands <ashsands1@amstrike.com>
2019-06-17 01:20:52 +0100

    Added BasePanGroup Class and a single input version

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/BasePanGroup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BasePanGroup : MonoBehaviour
+{
+	
+	public virtual int PanGroupId { get; set; }
+	protected InputHandler Inputs { get { return GameGlobals.inputs; } }
+	//protected ... HobGroupManager { get {GameGlobals.hobManager;} }	//TODO: 
+
+
+}
---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/BasePanGroup_singleInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AMS_Helpers;
+
+/// <summary>
+/// Base class for components that have a single input value
+/// </summary>
+public class BasePanGroup_singleInput : BasePanGroup
+{
+
+	[Header( "Input" )]
+	[SerializeField] private string inputName;
+	[SerializeField] private bool addPanId_prefix = false;
+
+	protected string InputName {
+		get { return inputName + ( addPanId_prefix ? "_" + PanGroupId : "" ); }
+	}
+	protected MinMax inputValue = new MinMax( 0, 1010 );
+
+	protected virtual void Update()
+	{
+
+		Inputs.GetInputValue( InputName, ref inputValue.current );
+
+	}
+
+}
