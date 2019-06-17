516d3c26a11940006dd2c3f98dedf031a948dce7
Ashley Sands <ashsands1@amstrike.com>
2019-06-17 01:21:42 +0100

    Added CookerKnobs Class
    rotates the hobKnob by its input value

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/CookerKnobs.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AMS_Helpers;
+
+public class CookerKnobs : BasePanGroup_singleInput
+{
+
+	[Tooltip("Start (min) and End (max) rotation of the hobKnob")]
+	[SerializeField] private MinMax rotationRange;
+
+
+
+	protected override void Update()
+	{
+
+		base.Update();
+		
+		Vector3 rotation = transform.eulerAngles;
+
+		rotation.z = rotationRange.GetValue( inputValue.Precent );
+
+		transform.eulerAngles = rotation;
+
+	}
+
+
+}
