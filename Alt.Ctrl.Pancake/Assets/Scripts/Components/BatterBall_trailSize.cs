5b7a8e69ed70b82fcfc0c2e383ca0df862e88eeb
Ashley Sands <ashsands1@amstrike.com>
2019-06-18 04:04:02 +0100

    add a class to chagnge the size of the batterballs trail size by it batter qt

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/BatterBall_trailSize.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Batter_quantity)), RequireComponent(typeof(TrailRenderer))]
+public class BatterBall_trailSize : MonoBehaviour
+{
+
+	private Batter_quantity batterBallQuantity;
+	private TrailRenderer trailRenderer;
+
+	[SerializeField] private float widthMutilplier = 1.5f;
+
+    void Awake()
+    {
+
+		batterBallQuantity = GetComponent<Batter_quantity>();
+		trailRenderer = GetComponent<TrailRenderer>();
+
+		batterBallQuantity.OnBatterAdded += OnBatterAdded;
+
+    }
+
+	private void OnBatterAdded( float amountAdded )
+	{
+
+		trailRenderer.widthMultiplier = batterBallQuantity.GetBatterPrecent() * widthMutilplier;
+
+	}
+}
