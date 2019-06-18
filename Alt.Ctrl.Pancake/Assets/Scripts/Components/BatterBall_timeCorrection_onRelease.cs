a411f7f3ac5c8a8380c9b08b278708210b48df42
Ashley Sands <ashsands1@amstrike.com>
2019-06-18 05:10:14 +0100

    Added class to correct the trails time once it has been released from the jug
    and it has a set time function as well

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/BatterBall_timeCorrection_onRelease.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TrailRenderer))]
+public class BatterBall_timeCorrection_onRelease : MonoBehaviour
+{
+
+	float spwanTime;
+
+    void Awake()
+    {
+		spwanTime = Time.time;
+    }
+
+	public void OnBatterRelease()
+	{
+
+		float timeDif = Time.time - spwanTime;
+
+		GetComponent<TrailRenderer>().time -= timeDif;
+
+	}
+
+	public void SetTime( float length)
+	{
+		GetComponent<TrailRenderer>().time = length;
+
+	}
+
+}
