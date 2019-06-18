b2b207ab5b3495cba450b805fc03825a65d9203d
Ashley Sands <ashsands1@amstrike.com>
2019-06-18 05:08:25 +0100

    Added class for random materialColor on start

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/RandomMaterialColor_OnStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class RandomMaterialColor_OnStart : MonoBehaviour
+{
+
+	[SerializeField] private string propertyName = "_Color";
+	[SerializeField] private Color colorA;
+	[SerializeField] private Color colorB;
+
+    void Start()
+    {
+		GetComponent<Renderer>().material.SetColor( propertyName, Color.Lerp( colorA, colorB, Random.value ) );
+    }
+
+
+}
