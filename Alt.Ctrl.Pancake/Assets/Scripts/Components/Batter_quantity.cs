e9845d007ddbd8239e879875fdfe44c95b63521c
Ashley Sands <ashsands1@amstrike.com>
2019-06-18 04:02:55 +0100

    Added batter quantity for batterball and jug batter

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/Batter_quantity.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Batter_quantity : BasePanGroup
+{
+
+	public delegate void OnBatter( float amount );
+	public event OnBatter OnBatterAdded;
+	public event OnBatter OnBatterUsed;
+
+	[SerializeField] private float maxBatterQuantity = 100;
+	[SerializeField] private bool setMaxOnStart = true;
+
+	private float currentBatterQuantity;
+
+	private void Start()
+	{
+		if(setMaxOnStart)
+			currentBatterQuantity = maxBatterQuantity;
+	}
+
+	/// <summary>
+	/// Add batter to quantiity
+	/// </summary>
+	/// <param name="amountOfBatterToAdd"></param>
+	/// <returns>returns remain batter over max</returns>
+	public float AddBatter(float amountOfBatterToAdd)
+	{
+
+		float remainingBatter = 0f;
+		currentBatterQuantity += amountOfBatterToAdd;
+
+		if ( currentBatterQuantity > maxBatterQuantity )
+		{
+			remainingBatter = currentBatterQuantity - maxBatterQuantity;
+			amountOfBatterToAdd -= remainingBatter;
+			currentBatterQuantity = maxBatterQuantity;
+		}
+
+		OnBatterAdded?.Invoke( amountOfBatterToAdd );
+
+		return remainingBatter;
+
+	}
+
+	/// <summary>
+	/// uses batter for it quantity and returns the amount of batter that is available.
+	/// </summary>
+	/// <param name="amountOfBatterToUse"></param>
+	/// <returns>The amount of batter that can be used.</returns>
+	public float UseBatter(float amountOfBatterToUse)
+	{
+
+		if ( currentBatterQuantity == 0 )							// No batter remaining :(
+		{
+			amountOfBatterToUse = 0;
+		}
+		else if ( currentBatterQuantity > amountOfBatterToUse )		// theres enought batter to fulfil the request :)
+		{ 
+			currentBatterQuantity -= amountOfBatterToUse;
+		}
+		else														// theres not enought batter for that, heres what remains :|
+		{
+			amountOfBatterToUse = currentBatterQuantity;
+			currentBatterQuantity = 0;
+		}
+
+		OnBatterUsed?.Invoke( amountOfBatterToUse );
+
+		return amountOfBatterToUse;
+
+	}
+
+	public bool HasBatter()
+	{
+		return currentBatterQuantity > 0f;
+	}
+
+	public float GetBatterQuantity()
+	{
+		return currentBatterQuantity;
+	}
+
+	public float GetBatterPrecent()
+	{
+		return currentBatterQuantity / maxBatterQuantity;
+	}
+
+}
