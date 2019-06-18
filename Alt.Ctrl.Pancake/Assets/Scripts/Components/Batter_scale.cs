b503d792c373e11a79534edc3d2fdd48e4a68d97
Ashley Sands <ashsands1@amstrike.com>
2019-06-18 05:07:36 +0100

    added BatterScale class and applied it to jug prefab

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/Batter_scale.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Batter_quantity))]
+public class Batter_scale : MonoBehaviour
+{
+
+	[SerializeField] private float maxYScale = 1f;
+	[SerializeField] private Transform scaleObj;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		GetComponent<Batter_quantity>().OnBatterChanged += OnBatterChanged;	//Since we return the remaining batter back to the jug when we use 
+    }
+
+	private void OnBatterChanged( float batterPrecentage )
+	{
+
+		Vector3 scale = scaleObj.localScale;
+		scale.y = maxYScale * batterPrecentage;
+		scaleObj.localScale = scale;
+
+	}
+
+}
