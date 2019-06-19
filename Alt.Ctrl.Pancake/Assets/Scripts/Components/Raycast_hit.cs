4cb2a2a90c27dcba4d3641781a6e1eab57bbbead
Ashley Sands <ashsands1@amstrike.com>
2019-06-19 06:03:36 +0100

    Added raycastHit and a version that works out the distance by it velocity

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/Raycast_hit.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Raycast_hit : MonoBehaviour
+{
+
+	public delegate void onRayHit( Vector3 hitLocation, Collider collider );
+	public event onRayHit OnRayHit;
+
+	[SerializeField] private RaycastHit rayHit;
+	[SerializeField] private LayerMask hitLayers;
+
+	[SerializeField] private Vector3 direction = Vector3.up;
+	[SerializeField] private bool useLocalDirection;
+
+	[SerializeField] protected float distance = 1f;
+
+	[Header("Debug")]
+	[SerializeField] private bool debug = false;
+	[SerializeField] private Color debug_lineColor = Color.red;
+
+
+
+	protected virtual void FixedUpdate()
+    {
+
+		if ( CastRay() )
+			OnRayHit?.Invoke( rayHit.point, rayHit.collider );
+
+    }
+
+	protected virtual bool CastRay()
+	{
+
+		bool hit = Physics.Raycast( transform.position, GetDirection().normalized, out rayHit, distance, hitLayers );
+
+		if ( debug )
+			Debug.DrawLine(transform.position, transform.position + (direction.normalized * distance), debug_lineColor );
+
+		return hit;
+
+	}
+
+	protected Vector3 GetDirection()
+	{
+		return useLocalDirection ? transform.TransformDirection(direction) : direction;
+	}
+
+}
---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/Raycast_hitVelocityDistance.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Raycast_hitVelocityDistance : Raycast_hit
+{
+
+	private Rigidbody rigid;
+
+	private void Awake()
+	{
+		rigid = GetComponent<Rigidbody>();
+	}
+
+	private void UpdateDistance()
+	{
+		// work out the velocity of the next move
+		Vector3 velocity = rigid.velocity;
+		velocity += Physics.gravity * Time.deltaTime;
+
+		// work out the max distance that are are going to move in in the next update
+		Vector3 nextPosition = transform.position + velocity;
+
+		distance = Vector3.Distance(transform.position, velocity);
+		
+	}
+
+}
