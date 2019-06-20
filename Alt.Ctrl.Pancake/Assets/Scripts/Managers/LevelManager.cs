679a4983239fe5c80dd042cfbfc9f65c6353de46
Ashley Sands <ashsands1@amstrike.com>
2019-06-20 22:48:53 +0100

    level manager now sets itself in gameGlobal

--- Alt.Ctrl.Pancake/Assets/Scripts/Managers/LevelManager.cs
+++ Alt.Ctrl.Pancake/Assets/Scripts/Managers/LevelManager.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-	   
-    }
+	public HobGroupManager hobGroupManager;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+	private void Awake()
+	{
+		GameGlobals.levelManager = this;
+	}
 }
