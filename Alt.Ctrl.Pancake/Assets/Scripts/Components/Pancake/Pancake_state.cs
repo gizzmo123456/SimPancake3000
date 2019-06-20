57fb50ca73c813d6d79322d7bd7587b50bded6c3
Ashley Sands <ashsands1@amstrike.com>
2019-06-20 02:29:10 +0100

    Added Pancake state to track the state of the pancake

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/Pancake/Pancake_state.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PancakeState { Mixture, Raw }
+
+public class Pancake_state : MonoBehaviour
+{
+
+	public delegate void onStateChange( PancakeState state );
+	public event onStateChange OnStateChanged;
+
+	private PancakeState pancakeState = PancakeState.Mixture;
+
+
+    void Update()
+    {
+        // state logic?
+    }
+
+	public PancakeState GetState()
+	{
+		return pancakeState;
+	}
+
+	public void SetState(PancakeState state)
+	{
+		pancakeState = state;
+
+		OnStateChanged?.Invoke(state);
+
+	}
+
+}
