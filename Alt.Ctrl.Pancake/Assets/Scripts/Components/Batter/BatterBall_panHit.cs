56930c8f0d9dfbcfc4dfc8a1a03d082f99337afa
Ashley Sands <ashsands1@amstrike.com>
2019-06-20 02:28:48 +0100

    added class to detect if batter has hit the pan using raycast
    by using a ray cast we can guarantee that the collider does not pass throught the pan without detecting the collisiion.
    
    and added it to the prefab
    
    (it was a rare occurrence in the alpha)

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/Batter/BatterBall_panHit.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Batter_quantity))]
+public class BatterBall_panHit : Raycast_hitVelocityDistance, IBatterRelease
+{
+
+	private bool updating = false;
+
+	protected override void FixedUpdate()
+	{
+		
+		if ( !updating ) return;
+
+		base.FixedUpdate();
+
+		if( CastRay() )
+			OnPanHit();
+	
+	}
+
+	private void OnPanHit( )
+	{
+		
+		// check that we have reached the pan. 
+		// the length of the raycast can be longer than the travel distance.
+		if ( NextPosition.y > rayHit.point.y ) return;
+
+		// correct it position, turn off gravity and the raycast since we have now reached the pan, and we wont be colliding with anythink else.
+		updating = false;
+		GetComponent<Rigidbody>().useGravity = false;
+		transform.position = rayHit.point;
+
+
+		//Attamp to find the fryingPan Pancake class, it has to search its parents since the collider is nested into the object.
+		FryingPan_pancake panPancake = rayHit.transform.GetComponentInParent<FryingPan_pancake>();
+
+		if ( panPancake == null ) return;
+
+		// Get batter qt of the batterBall and apply it to a mixture pancake.
+		Batter_quantity quantity = GetComponent<Batter_quantity>();
+
+		panPancake.AddBatter( quantity.GetBatterQuantity(), rayHit.point );
+
+	}
+
+	public void OnBatterRelease()
+	{	// we only need update once we have been released for the jug :)
+		updating = true;
+	}
+}
