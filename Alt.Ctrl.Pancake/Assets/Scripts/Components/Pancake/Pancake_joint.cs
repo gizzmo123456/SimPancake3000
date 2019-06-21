a39012a62ef11ed515a5d16147164860cd486b0c
Ashley Sands <ashsands1@amstrike.com>
2019-06-21 02:43:00 +0100

    Added pancake collision class
    it sets up all the pancake joints and update currect pan on all joints.
    
    a joint basicly rotates and moves depending on the distance from the center of the current pan

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/Pancake/Pancake_joint.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class to manipulate the Z rotation of pancake joints.
+/// </summary>
+public class Pancake_joint : MonoBehaviour, IPanCollider
+{
+
+	// TODO: ...
+
+
+	// tracking object.
+	private Transform panColliderObj;
+
+	// collider set up.
+	private AnimationCurve colliderCurve;			// this should be in the range of X: 0,1; Y: 0,1;
+	private float maxDistanceFromCenter;            // X axis of curve
+
+	// joint constains
+	private float maxCurveRotation = 90f;            // y axis of curve.
+	private float maxPositionOffset = 0.05f;
+	// ...
+
+	private Vector3 TEMP_pos;
+	private float TEMP_DIST;
+
+    private void Update()
+    {
+		if ( panColliderObj == null ) return;
+
+		// rotate the joint based on the distance from the cneter of the panColliderObj.
+		// ignoreing the Y axis.
+
+		Vector3 position = transform.position;
+		Vector3 panPosition = panColliderObj.transform.position;
+
+		position.y = panPosition.y = 0;
+
+		float distanceFromCenter = TEMP_DIST = Vector3.Distance( position, panPosition );
+		float distancePercent = distanceFromCenter / maxDistanceFromCenter;
+
+		// Get the rotation from the colliderCurve.
+		Vector3 rotation = transform.eulerAngles;
+
+		float curveValue = colliderCurve.Evaluate( distancePercent );
+		rotation.z = curveValue * maxCurveRotation;
+
+		transform.eulerAngles = rotation;
+
+		// update the position offset.
+		// local
+		Vector3 lPos = TEMP_pos;
+		lPos.y +=  curveValue * maxPositionOffset ;
+	
+		transform.localPosition = lPos;
+
+		// world 
+		Vector3 wPos = Vector3.zero;
+		wPos.y = curveValue * maxPositionOffset;
+
+		transform.position += wPos;
+		
+	}
+
+	public void SetPanCollider( Transform panColl )
+	{
+		panColliderObj = panColl;
+		TEMP_pos = transform.localPosition;
+	}
+
+	public void SetupColliderData( AnimationCurve collCurve, float maxDistance )
+	{
+		colliderCurve = collCurve;
+		maxDistanceFromCenter = maxDistance;
+	}
+
+	public void SetupJointData( float curveRotation )
+	{
+		// ...
+		maxCurveRotation = curveRotation;
+	}
+}
---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/Pancake/Pancake_panCollision.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pancake_panCollision : MonoBehaviour, IPanCollider
+{
+
+	private Transform panColliderObj;							// NOTE: On a reflection i this should just be a transform, i dont think it needs its own class class or anythink :)
+	private Pancake_joint[] joints;
+
+
+	[SerializeField] private AnimationCurve colliderCurve;
+	[SerializeField] private float maxDistanceFromCenter;
+
+	[SerializeField] private float maxJointRotation;
+	[SerializeField] private float maxPositionOffset;
+	[Range(0f, 1f)]
+	[SerializeField] private float[] jointDepthWeight;
+
+	private void Awake()
+	{
+
+		joints = FindChildrenWithJoints( transform ).ToArray();
+
+	}
+
+	/// <summary>
+	/// find all the children with joint in the.
+	/// </summary>
+	/// <param name="parent">transform to search</param>
+	/// <param name="nestedId">the depth of child in hierarchy from objects root</param>
+	/// <returns></returns>
+	List<Pancake_joint> FindChildrenWithJoints(Transform parent, int nestedId = 0)		// TODO: i havent set the depth anywhere yet :|
+	{
+		List<Pancake_joint> pancakeJoints = new List<Pancake_joint>();
+
+		for (int i = 0; i < parent.childCount; i++ )
+		{
+			Transform child = parent.GetChild( i );
+
+			if ( child.childCount > 0 )
+				pancakeJoints.AddRange( FindChildrenWithJoints( child, nestedId + 1) );
+
+			if ( !child.name.Contains( "joint" ) ) continue;
+
+			float jointWeight = nestedId < jointDepthWeight.Length ? jointDepthWeight[ nestedId ] : 1f;
+
+			Pancake_joint childJoint = child.gameObject.AddComponent<Pancake_joint>();
+			childJoint.SetupColliderData(colliderCurve, maxDistanceFromCenter);
+			childJoint.SetupJointData(maxJointRotation * jointWeight, maxPositionOffset);
+
+			pancakeJoints.Add( childJoint );
+			
+		}
+
+		return pancakeJoints;
+
+	}
+
+	public void SetPanCollider(Transform gameObj)
+	{
+
+		panColliderObj = gameObj;
+
+		//Update all the children joints
+		foreach ( Pancake_joint joint in joints )
+			joint.SetPanCollider( gameObj );
+
+	}
+
+
+}
