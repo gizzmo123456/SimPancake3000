da584a95618b988f1c7cd17f3fc8425d23ac459e
Ashley Sands <ashsands1@amstrike.com>
2019-06-21 04:14:58 +0100

    Renamed Pancake_collision to pancake_jointSetup and pancake_panCollision will be the main pancakes collision system
    makes sence right?

--- Alt.Ctrl.Pancake/Assets/Scripts/Components/FryingPan/FryingPan_pancake.cs
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/FryingPan/FryingPan_pancake.cs
@@ -26,7 +26,7 @@ public class FryingPan_pancake : BasePanGroup
 		{
 			Pancake_state pancake = Instantiate( pancakePrefab, hitLocaltion, Quaternion.identity );
 			pancake.GetComponent<Batter_quantity>().AddBatter(qt);
-			pancake.GetComponent<Pancake_panCollision>().SetPanCollider(panColliderObj);
+			pancake.GetComponent<Pancake_jointSetup>().SetPanCollider(panColliderObj);
 
 			// if there are already pancake in the pan combine them into the new pancake.
 			// TODO: this should only be if the mixture coms into contact this a non mixture pancake.
