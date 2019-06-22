2f5180a83d7bd324d2044a9606ad5ef1f92b6087
Ashley Sands <ashsands1@amstrike.com>
2019-06-22 02:41:58 +0100

    added Script to help with debuging the pancakes

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/Pancake/Pancake_DEBUG.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pancake_DEBUG : MonoBehaviour
+{
+
+	public GameObject pancake;
+	public Transform panCollider;
+	public bool setPanCollider = false;
+	public bool updatePosition = false;
+
+    void Update()
+    {
+
+		if ( pancake == null ) return;
+
+		if( setPanCollider )
+		{
+			pancake.SendMessage("SetPanCollider", panCollider);
+			setPanCollider = false;
+		}
+
+		if ( updatePosition )
+		{
+			Pancake_panCollision panColl = pancake.GetComponent<Pancake_panCollision>();
+
+			if( panColl.GetPanCollider() != null )
+				panColl.positionInPan = panColl.GetPanCollider().InverseTransformPoint(transform.position);
+		}
+    }
+}
