6ab4fe525cc610b7886a856e26ada3456dbf9d1d
Ashley Sands <ashsands1@amstrike.com>
2019-06-22 04:21:39 +0100

    Add IVelocity Interface for anythink that can have velocity added to it

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Interfaces/IVelocity.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IVelocity
+{
+	Vector3 Velocity { get; }
+
+	void AddVelocity( Vector3 vel );
+	void SetVelocity( Vector3 vel );
+	Vector3 GetVelocity( );
+
+}
