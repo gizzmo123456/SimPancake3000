755c55880912719e0a379f37b9e07053f9c4ffae
Ashley Sands <ashsands1@amstrike.com>
2019-06-23 03:06:53 +0100

    Extended Vector3's to have a Multiply function

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Helpers/ExtendVector3.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtendVector3
+{
+	public static Vector3 Multiply(this Vector3 vector, Vector3 multiValue)
+	{
+		Vector3 muli = vector;
+		muli.x *= multiValue.x;
+		muli.y *= multiValue.y;
+		muli.z *= multiValue.z;
+
+		return muli;
+	}
+}
