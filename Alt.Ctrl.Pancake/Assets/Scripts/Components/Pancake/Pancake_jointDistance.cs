9a43bb00ae312191151f9658b952b03f0a74c8f7
Ashley Sands <ashsands1@amstrike.com>
2019-06-23 03:15:12 +0100

    changed jointDistance to now store it start position in local space of its upmost parent (in prefab)
    since it is more consistent to uses distance from cent when the pancake is flat as it can bend inwards.
    Still need to clear this up a lil as well
    This also need to be incorerated into joint, i think this is much more precise

--- Alt.Ctrl.Pancake/Assets/Scripts/Components/Pancake/Pancake_jointDistance.cs
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/Pancake/Pancake_jointDistance.cs
@@ -9,32 +9,54 @@ public class Pancake_jointDistance : MonoBehaviour, IPanCollider
 	[SerializeField] private Pancake_panCollision panCollision;
 	private Transform panColliderObj;
 
+	private Transform originTranform; // or top-most parent in prefab.
+	private Vector3 localOrigin;
+	private Vector3 WORLDorigin;
+
+	public float TEMPDIST = 0;
+
 	private void Update()
 	{
 
 		if ( panColliderObj == null ) return;
 
 		// find how far from the center we are
-		Vector3 position = transform.position;
+		Vector3 originPosition = WORLDorigin = originTranform.TransformPoint( localOrigin ); //transform.position;
 		Vector3 panPosition = panColliderObj.transform.position;
 
-		position.y = panPosition.y = 0;
+		originPosition.y = panPosition.y = 0;
 
-		float distance = Vector3.Distance( position, panPosition );
+		float distance = TEMPDIST = Vector3.Distance( originPosition, panPosition );
 
 		// if we are past the max distance attampt to apply upforce/flip :)
 		if ( distance > maxDistanceFromCenter )
-			panCollision.TransformToUpforce(transform.forward);
-
+		{
+			panCollision.TransformToUpforce( -transform.right, distance, name );      //the joint have been orrentated so that left is forwards, not ideal but thats just how it is. my myay skills are not the best! :|
+			Debug.Log( "Trandform to up, name: " + name + " ## Dist: " + distance + " ## Frw: " + ( -transform.right ), gameObject );
+		}
 	}
 
-	public void SetPanCollider( Transform panColl )
+	public void SetPanCollider( Transform panCollObj)
 	{
-		panColliderObj = panColl;
+		panColliderObj = panCollObj;
 	}
 
-	public void Setup( float maxDist )
+	public void Setup( Transform origin, float maxDist, Pancake_panCollision panColl )
 	{
+		// get the position of this joint localy to its top-most parent (in terms of prefab).
+		originTranform = origin;
+
+		// store the current scale of the origin so we can get the joints position in the origins local space when the scale is 1,1,1 .
+		// then return it back to the original scale. (maybe not the best way to do this but proberly the simperlist)
+		Vector3 originScale = origin.localScale;
+		origin.localScale = Vector3.one;
+		localOrigin = originTranform.InverseTransformPoint( transform.position );
+		origin.localScale = originScale;
+
+		Debug.Log( "SETUP FOR: " + name + " wPos:" + transform.position, gameObject );
+
 		maxDistanceFromCenter = maxDist;
+		panCollision = panColl;
 	}
+
 }
