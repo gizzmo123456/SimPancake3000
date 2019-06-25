7e04f2ed66bee6474146466675b874c2787ca226
Ashley Sands <ashsands1@amstrike.com>
2019-06-25 02:15:27 +0100

    Add class to store refs to the main comps of the frying pan just to make life a lil easyer :)

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/FryingPan/FryingPan.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FryingPan : MonoBehaviour
+{
+	public FryingPan_rotation rotation;
+	public FryingPan_hobPosition hobPosition;
+	public FryingPan_pancake pancake;
+	public FryingPan_force force;
+
+}
