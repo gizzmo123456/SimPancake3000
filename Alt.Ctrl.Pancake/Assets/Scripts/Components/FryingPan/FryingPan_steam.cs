52ced0cc6e42cb7a22d24363df32c9ed8ff34533
Ashley Sands <ashsands1@amstrike.com>
2019-07-03 19:34:28 +0100

    Added Class for dynamic pan steam
    base on pan temp

---
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/FryingPan/FryingPan_steam.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AMS_Helpers;
+
+[RequireComponent(typeof( FryingPan_temperature ))]
+public class FryingPan_steam : MonoBehaviour
+{
+
+	private FryingPan_temperature panTemperature;
+
+	[SerializeField] private ParticleSystem steamParticleEffect;
+	private ParticleSystem.EmissionModule particleEffectEmissions;
+	private ParticleSystem.MainModule particleEffectMain;
+
+	[SerializeField] private MinMax temperatureRange = new MinMax( 100, 300 );
+	[SerializeField] private MinMax particleMultiplierRange = new MinMax( 0f, 1f );
+
+	[SerializeField] private Gradient colorRange = new Gradient(); 
+
+
+
+
+	private void Awake()
+	{
+		panTemperature = GetComponent<FryingPan_temperature>();
+		particleEffectEmissions = steamParticleEffect.emission;
+		particleEffectMain = steamParticleEffect.main;
+	}
+
+	private void Update()
+	{
+
+		if ( steamParticleEffect == null ) return;
+
+		temperatureRange.current = panTemperature.GetCurrentTemprature();
+
+		particleEffectEmissions.rateOverTimeMultiplier = particleMultiplierRange.GetValue( temperatureRange.ClampedPrecent );
+		particleEffectMain.startColor = colorRange.Evaluate( temperatureRange.ClampedPrecent );
+
+	}
+
+}
