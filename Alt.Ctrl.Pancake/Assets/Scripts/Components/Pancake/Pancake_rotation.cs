8a769e3a4f62696a80f9c344d4bb67b766932d94
Ashley Sands <ashsands1@amstrike.com>
2019-07-09 02:55:35 +0100

    Pancake flip now levels off once a thresshold has been reached

--- Alt.Ctrl.Pancake/Assets/Scripts/Components/Pancake/Pancake_rotation.cs
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/Pancake/Pancake_rotation.cs
@@ -11,6 +11,10 @@ public class Pancake_rotation : MonoBehaviour, IPanCollider
 	private Transform rotateObj;
 
 	[SerializeField] private float rotateSpeed = 180f; // per second
+	private float flipSpeed = 0;
+	[Tooltip("When the rotate speed drops below the delta thresshold it will attampt to level the pancake out to next 180 degree (ie. 0/360 or 180")]
+	[SerializeField] private float flipSpeedDeltaThresshold = 5f;
+	private float targetFinishRotation = -1;				  // <0 is unset. 
 
 	private void Awake()
 	{
@@ -30,15 +34,45 @@ public class Pancake_rotation : MonoBehaviour, IPanCollider
     {
 
 		if ( panColliderObj != null ) return;   // dont rotate if we are in the pan.
-
-		if ( velocity.GetVelocity().y <= 0 ) return;
-
-		// move the pancake into the rot object and rotate around the zAxis,
-		// and remove pancake from rotate object.
-		// this alows us to rotate the pancane in the same direction as the force when leaving the pan :)
+		if ( velocity.GetVelocity().y == 0 ) return;
 
 		Vector3 currentVelocity = velocity.GetVelocity();
-		float flipSpeed = ( currentVelocity.y / ( ( Mathf.Abs( currentVelocity.x ) + Mathf.Abs( currentVelocity.z ) ) / 2f ) ) * rotateSpeed; //per sec;
+
+		// Work out the flip speed.
+		// level the pancake out to the next 180degrees if the pancake is falling or below the flipspeed thresshold
+		// else work out the flip speed by the pancakes velocity.
+		// This will give us a dynamic flip velocity and will make it less likely that the pancake will land verticaly.
+		// giving the player a better chance of compleating a flip :)
+
+		if ( flipSpeed < 0f && ( currentVelocity.y < 0 || flipSpeed > -flipSpeedDeltaThresshold ) )
+		{
+			if ( targetFinishRotation < 0 )
+				UpdateTargetFinishRotation();
+
+			// find the differeance between our current rotation and targetFinishRotation.
+			float targetDif = targetFinishRotation - AMS_Helpers.Maf.ClampRotation(rotateObj.eulerAngles.z);
+
+			// we must make sure that we do NOT exceed the current flipSpeed. 
+			// we do not want the pancakes rotation to randomly speed up
+			if ( -targetDif < flipSpeed )
+				flipSpeed -= flipSpeed * Time.deltaTime;		// slowly decress the flip speed so it can come to a hult.
+			else
+				flipSpeed = -targetDif;                          // this will naturally slow down, the closer we get to the target.
+
+			print( "goingToTarget" );
+
+		}
+		else if ( currentVelocity.y > 0f )
+		{
+			print( "fliping" );
+			flipSpeed = ( currentVelocity.y / ( ( Mathf.Abs( currentVelocity.x ) + Mathf.Abs( currentVelocity.z ) ) / 2f ) ) * rotateSpeed; //per sec;
+
+		}
+
+		print( " FlipSpeed: " + flipSpeed );
+		// move the pancake into the rot object and rotate around the zAxis, and remove pancake from rotate object.
+		// this alows us to rotate the pancane in the same direction as the force when leaving the pan :)
+		// The rotate object is oriantated when the flip starts in SetFlipRotation :D
 
 		Vector3 nextRot = rotateObj.eulerAngles;
 		nextRot.z += flipSpeed * Time.deltaTime;
@@ -52,7 +86,7 @@ public class Pancake_rotation : MonoBehaviour, IPanCollider
 
 		transform.parent = null;
 		
-		// TODO: level flip off when below flipspeed thresshold??
+		// TODO: level flip off when below flipspeed thresshold?? DONE??
 
     }
 
@@ -73,6 +107,30 @@ public class Pancake_rotation : MonoBehaviour, IPanCollider
 
 		rotateObj.LookAt( rotateObj.position + velNorm );
 
+		// reset the target rotation, so we know it needs to be work out again once we are below the flip thresshold or falling.
+		targetFinishRotation = -1;
+		flipSpeed = 0;
+
+	}
+
+	private void UpdateTargetFinishRotation()
+	{
+
+		// find the next rotation that is a mulitple of 180 (ie 0/360 or 180)
+
+		// TODO: take the current flip speed into account. if it is above the thresshold (this is posible if we start falling befor the thresshold is reached)
+		// the target finish should be the mulitple of 180 affter the next.
+
+		// P.S we only rotation on the Z axis. :)
+
+		float currentZRotation = AMS_Helpers.Maf.ClampRotation( rotateObj.eulerAngles.z );
+
+		// Find how far past the last multiple of 180 we are so it can be taken away from the current rotation 
+		// then add 180 for the next mutilple of 180 :)
+		float remainingRotation = currentZRotation % 180;
+
+		targetFinishRotation = currentZRotation - remainingRotation + 180f;
+		
 	}
 
 	private void OnDestroy()
