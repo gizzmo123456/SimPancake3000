832aa7bc9d6a90ab4347d47e221a09f110658160
Ashley Sands <ashsands1@amstrike.com>
2019-07-12 02:17:04 +0100

    Add physic friction to pancake velocity
    it take the angle into acount

--- Alt.Ctrl.Pancake/Assets/Scripts/Components/Pancake/Pancake_velocity.cs
+++ Alt.Ctrl.Pancake/Assets/Scripts/Components/Pancake/Pancake_velocity.cs
@@ -17,8 +17,10 @@ public class Pancake_velocity : MonoBehaviour, IVelocity
 	private bool physicsEnabled = true;
 	private Batter_quantity batterQuantity;
 	[SerializeField] private Transform updateSpace;         // space that velocity is oriented to. if null then world space. //TODO:	// DOES this even get used??
+	[SerializeField] private Vector3 physicFriction = Vector3.zero;
 	[Tooltip("Keep values positive. negative values can cause unexpected issues. the limited velocity range is '-limit, limit' ")]
 	[SerializeField] private Vector3 limitedVelocity_limit = Vector3.one;
+
 
 	private Vector3 velocity = Vector3.zero;
 	private Vector3 limitedVelocity = Vector3.zero;
@@ -124,6 +126,8 @@ public class Pancake_velocity : MonoBehaviour, IVelocity
 
 		velocity += Physics.gravity * delta;
 
+		PhysicsFriction();
+
 	}
 
 	public void SetUpdateSpace( Transform spaceTrans )	//NOTE: dont know if this is being called from anywhere, but all seems to be working just fine :)
@@ -155,39 +159,114 @@ public class Pancake_velocity : MonoBehaviour, IVelocity
 		else return updateSpace.InverseTransformVector( worldPosition );
 	}
 
-	public void AddFriction( float velocityFriction, float limitedVelocityFriction )
+	/// <summary>
+	/// Takes the pancakes rotation into account.
+	/// </summary>
+	private void PhysicsFriction()
 	{
 
-		AddFriction( ref velocity, velocityFriction );			// friction for default velocity
-		AddFriction( ref limitedVelocity, velocityFriction );	// friction for limited velocity
+		Vector3 friction = Vector3.zero;
+
+		// dont need the Y axis, it just spins thus does not affect friction
+		Vector3 mod180 = Vector3.zero;
+		mod180.x = transform.eulerAngles.x % 180f;
+		mod180.z = transform.eulerAngles.z % 180f;
+
+		// By adding a 90 deg offset to the mod180 and takeing 180 we get the rotation in a rage of -90 & 90, 0 being 90 deg.
+		// so if do this for the X and Z frict we can then chose the lowers ABS value for the Y (vert) Friction (90), then all we have 
+		// to do is take friction value for X/Z away from 90 for the hoz friction (180).
+
+		friction.x = Mathf.Abs( mod180.x + 90f - 180f );
+		friction.z = Mathf.Abs( mod180.z + 90f - 180f );
+
+		// find the lowest for the Y friction
+		// TODO: maybe we could make this better than lowest. Projection ?
+		friction.y = friction.x < friction.z ? friction.x : friction.z;
+
+		// take the friction from 90 for the hoz friction (180)
+		friction.x = 90f - friction.x;
+		friction.z = 90f - friction.z;
+
+		// turn it into a percentage.
+		friction.x /= 90f;
+		friction.y /= 90f;
+		friction.z /= 90f;
+
+		// and finaly mutiple it by the physics friction :D
+		friction = friction.Multiply( physicFriction );
+
+		AddFriction( ref velocity, friction );
 
 	}
 
+	/// <summary>
+	/// Add friction to both the velocity and limited valocity. 
+	/// only affecting X and Z axis.
+	/// </summary>
+	/// <param name="velocityFriction"></param>
+	/// <param name="limitedVelocityFriction"></param>
+	public void AddFriction( float velocityFriction, float limitedVelocityFriction )
+	{
+
+		AddFriction( ref velocity, velocityFriction );          // friction for default velocity
+		AddFriction( ref limitedVelocity, velocityFriction );   // friction for limited velocity
+
+	}
+
+	/// <summary>
+	/// Add friction to X/Z axis only.
+	/// </summary>
+	/// <param name="vel"></param>
+	/// <param name="friction"></param>
 	private void AddFriction( ref Vector3 vel, float friction )
-	{ 
+	{
 
 		// aka counter force x, z
-		if ( vel.x < 0 )
+		vel.x = StopAtZero( vel.x, friction * Time.deltaTime );
+		vel.z = StopAtZero( vel.z, friction * Time.deltaTime );
+
+	}
+
+	/// <summary>
+	/// Added friction to each axis (x, y, z)
+	/// </summary>
+	/// <param name="vel"></param>
+	/// <param name="friction"></param>
+	private void AddFriction( ref Vector3 vel, Vector3 friction )
+	{
+
+		vel.x = StopAtZero( vel.x, friction.x * Time.deltaTime );
+		vel.y = StopAtZero( vel.y, friction.y * Time.deltaTime );
+		vel.z = StopAtZero( vel.z, friction.z * Time.deltaTime );
+
+	}
+
+	/// <summary>
+	/// If value is grater than zero value 2 is taken away. if value is less than zero value is added. WILL NOT cross over zero.
+	/// </summary>
+	private float StopAtZero(float value, float value2) // what the hell do you call a function that does this.
+	{
+
+		if( value < 0 )
 		{
-			vel.x += friction * Time.deltaTime;
-			if ( vel.x > 0 ) vel.x = 0;
+
+			value += value2;
+
+			if ( value > 0 )
+				value = 0;
+
 		}
-		else if ( vel.x > 0 )
+		else if( value > 0)
 		{
-			vel.x -= friction * Time.deltaTime;
-			if ( vel.x < 0 ) vel.x = 0;
+
+			value -= value2;
+
+			if ( value < 0 )
+				value = 0;
+			
 		}
 
-		if ( vel.z < 0 )
-		{
-			vel.z += friction * Time.deltaTime;
-			if ( vel.z > 0 ) vel.z = 0;
-		}
-		else if ( vel.z > 0 )
-		{
-			vel.z -= friction * Time.deltaTime;
-			if ( vel.z < 0 ) vel.z = 0;
-		}
+		return value;
 
 	}
 
